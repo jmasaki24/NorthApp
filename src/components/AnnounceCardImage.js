15318030fa2c5395d2d296919aea288da54f592d
jmasaki24 <jamie.m.maddock@gmail.com>
2018-09-05 08:24:33 -0400

    not much

--- src/components/AnnounceCardImage.js
+++ src/components/AnnounceCardImage.js
@@ -4,12 +4,12 @@ import { Card, CardSection } from './common';
 
 const AnnounceCardImage = ({ title, uri, children }) => (
     <Card>
-      <CardSection style={{ justifyContent: 'center' }}>
+      <CardSection style={{ justifyContent: 'center', alignItems: 'center' }}>
         <Text style={{ fontSize: 25, fontWeight: 'bold', }}>
           {title}
         </Text>
       </CardSection>
-      <CardSection>
+      <CardSection style={{ alignItems: 'center' }}>
         <View style={{ flex: 1 }}>
           <Image
             style={{ width: 140, height: 140, alignSelf: 'center' }}
