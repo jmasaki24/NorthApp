c4ca56f98d39ad4118682fd9b1a14c569cc807ba
jmasaki24 <jamie.m.maddock@gmail.com>
2018-10-02 22:03:41 -0400

    style menu no card

--- src/components/MenuPage.js
+++ src/components/MenuPage.js
@@ -3,7 +3,7 @@ import { View, Image, Text, TouchableOpacity } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import Titan from '../images/titanT.png';
-import { CardSection, Button, Card } from './common';
+import { CardSection, Button, } from './common';
 import ContactPage from './ContactPage';
 import AcademicsPage from './AcademicsPage';
 import ClubsPage from './Clubs';
@@ -18,66 +18,63 @@ class MenuPage extends Component {
   render() {
     const { buttonStyle, textStyle, pageStyle } = styles;
     return (
-
-          <Card>
-            <CardSection style={{ padding: 0 }}>
-              <Button
-                buttonStyle={buttonStyle}
-                textStyle={textStyle}
-                onPress={() => this.props.navigation.navigate('Athletics')}
-              >
-                Athletics
-              </Button>
-            </CardSection>
-            <CardSection style={{ padding: 0 }}>
-              <Button
-                buttonStyle={buttonStyle}
-                textStyle={textStyle}
-                onPress={() => this.props.navigation.navigate('Academics')}
-              >
-                Academics
-              </Button>
-            </CardSection>
-            <CardSection style={{ padding: 0 }}>
-              <Button
-                buttonStyle={buttonStyle}
-                textStyle={textStyle}
-                onPress={() => this.props.navigation.navigate('Store')}
-              >
-                Store
-              </Button>
-            </CardSection>
-            <CardSection style={{ padding: 0 }}>
-              <Button
-                buttonStyle={buttonStyle}
-                textStyle={textStyle}
-                onPress={() => this.props.navigation.navigate('Clubs')}
-              >
-                Clubs
-              </Button>
-            </CardSection>
-            <CardSection style={{ padding: 0 }}>
-              <Button
-                buttonStyle={buttonStyle}
-                textStyle={textStyle}
-                onPress={() => this.props.navigation.navigate('Contact')}
-              >
-                Contact Us
-              </Button>
-            </CardSection>
-            <CardSection style={{ padding: 0 }}>
-              <Button
-                buttonStyle={buttonStyle}
-                textStyle={textStyle}
-                onPress={() => this.props.navigation.navigate('Admin')}
-              >
-                Admin
-              </Button>
-            </CardSection>
-            <Text style={{ alignSelf: 'center' }}>Copyright Nashua North 2018</Text>
-          </Card>
-
-
+      <View style={pageStyle}>
+        <CardSection style={{ padding: 0 }}>
+          <Button
+            buttonStyle={buttonStyle}
+            textStyle={textStyle}
+            onPress={() => this.props.navigation.navigate('Athletics')}
+          >
+            Athletics
+          </Button>
+        </CardSection>
+        <CardSection style={{ padding: 0 }}>
+          <Button
+            buttonStyle={buttonStyle}
+            textStyle={textStyle}
+            onPress={() => this.props.navigation.navigate('Academics')}
+          >
+            Academics
+          </Button>
+        </CardSection>
+        <CardSection style={{ padding: 0 }}>
+          <Button
+            buttonStyle={buttonStyle}
+            textStyle={textStyle}
+            onPress={() => this.props.navigation.navigate('Store')}
+          >
+            Store
+          </Button>
+        </CardSection>
+        <CardSection style={{ padding: 0 }}>
+          <Button
+            buttonStyle={buttonStyle}
+            textStyle={textStyle}
+            onPress={() => this.props.navigation.navigate('Clubs')}
+          >
+            Clubs
+          </Button>
+        </CardSection>
+        <CardSection style={{ padding: 0 }}>
+          <Button
+            buttonStyle={buttonStyle}
+            textStyle={textStyle}
+            onPress={() => this.props.navigation.navigate('Contact')}
+          >
+            Contact Us
+          </Button>
+        </CardSection>
+        <CardSection style={{ padding: 0 }}>
+          <Button
+            buttonStyle={buttonStyle}
+            textStyle={textStyle}
+            onPress={() => this.props.navigation.navigate('Admin')}
+          >
+            Admin
+          </Button>
+        </CardSection>
+        <Text style={{ alignSelf: 'center' }}>Copyright Nashua North 2018</Text>
+      </View>
     );
   }
 }
