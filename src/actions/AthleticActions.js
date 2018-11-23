63555aeda9a7f72b8e9e0fbef57b7b6e1627243e
petermatts <mpeters0514@gmail.com>
2018-11-23 01:25:40 -0500

    upcoming games page now displays upcoming games, much styling needed!

--- src/actions/AthleticActions.js
+++ src/actions/AthleticActions.js
@@ -9,43 +9,44 @@ import {
 
 export const getUpcomingGames = () => {
   const finalArray = [];
-  axios.get(UPCOMING_GAMES_URL)
-  .then((response) => {
-    if (response.status === 200) {
-      const $ = cheerio.load(response.data);
-      const upcomingGames = [];
-      const games = $('.schedule-game a');
-      //console.log(games);
+  return (dispatch) => {
+    axios.get(UPCOMING_GAMES_URL)
+      .then((response) => {
+        if (response.status === 200) {
+          const $ = cheerio.load(response.data);
+          const upcomingGames = [];
+          const games = $('.schedule-game a');
+          //console.log(games);
 
-      for (let i = 0; i < games.length; i++) {
-        //console.log(games[i].parent.attribs.class);
-        if (games[i].parent.attribs.class === 'schedule-game  js-schedule-game schedule-game--timeline-future') {
-          upcomingGames.push(games[i]);
-        }
-      } //sort all games in list into upcoming games
-      //console.log(upcomingGames);
+          for (let i = 0; i < games.length; i++) {
+            //console.log(games[i].parent.attribs.class);
+            if (games[i].parent.attribs.class === 'schedule-game  js-schedule-game schedule-game--timeline-future') {
+              upcomingGames.push(games[i]);
+            }
+          } //sort all games in list into upcoming games
+          //console.log(upcomingGames);
 
-      for (let i = 0; i < upcomingGames.length; i++) {
-        const len = upcomingGames[i].children.length;
-        //console.log(len);
-        const sportArr = [];
+          for (let i = 0; i < upcomingGames.length; i++) {
+            const len = upcomingGames[i].children.length;
+            //console.log(len);
+            const sportArr = [];
 
-        for (let j = 1; j < len; j += 2) {
-          const text = upcomingGames[i].children[j].children[0].data;
-          //console.log(text);
-          const key = upcomingGames[i].children[j].attribs.class;
-          //console.log(key);
-          if (!text.includes('Click for more details!')) {
-            sportArr.push([key.trim(), text]);
+            for (let j = 1; j < len; j += 2) {
+              const text = upcomingGames[i].children[j].children[0].data;
+              //console.log(text);
+              const key = upcomingGames[i].children[j].attribs.class;
+              //console.log(key);
+              if (!text.includes('Click for more details!')) {
+                sportArr.push([key.trim(), text]);
+              }
+            }
+            finalArray.push(sportArr);
           }
         }
-        finalArray.push(sportArr);
-      }
-    }
-  });
-
-  return {
-    type: GET_UPCOMING_GAMES,
-    payload: finalArray
+        dispatch({
+          type: GET_UPCOMING_GAMES,
+          payload: finalArray
+        });
+      });
   };
 };
--- src/components/AthleticsDetails/UpcomingGamesPage.js
+++ src/components/AthleticsDetails/UpcomingGamesPage.js
@@ -6,7 +6,7 @@
 import React, { Component } from 'react';
 import { View, Text, FlatList } from 'react-native';
 import { connect } from 'react-redux';
-import { Card, CardSection } from '../common';
+import { CardSection } from '../common';
 import { getUpcomingGames } from '../../actions';
 
 class UpcomingGamesPage extends Component {
@@ -15,48 +15,90 @@ class UpcomingGamesPage extends Component {
     console.log(this.props.upcomingGames);
   }
 
-  renderItem(item) {
+  renderItem({ item }) {
     console.log(item);
+    let sport = 'N/A';
+    let otherTeam = 'N/A';
+    let time = 'N/A';
+    const len = item.length;
+
+    for (let i = 0; i < len; i++) {
+      if (item[i][0] === 'schedule-game__sport') {
+        sport = item[i][1];
+      } else if (item[i][0] === 'schedule-game__opponent') {
+        otherTeam = item[i][1];
+      } else if (item[i][0] === 'schedule-game__time') {
+        time = item[i][1];
+      }
+    }
+
     return (
-      null
+      <CardSection style={styles.bodyCardSectionStyle}>
+        <View style={styles.leftViewStyle}>
+          <Text style={styles.bodyTextStyle}>{sport}</Text>
+        </View>
+        <View style={styles.middleViewStyle}>
+          <Text style={styles.bodyTextStyle}>{otherTeam}</Text>
+        </View>
+        <View style={styles.rightViewStyle}>
+          <Text style={styles.bodyTextStyle}>{time}</Text>
+        </View>
+      </CardSection>
     );
   }
 
   render() {
+    console.log(this.props.upcomingGames);
     return (
-      <View>
-        <Card>
-          <CardSection>
-            <View style={styles.headerViewStyle}>
-              <Text style={styles.headerTextStyle}>Sport</Text>
-            </View>
-            <View style={styles.headerViewStyle}>
-              <Text style={styles.headerTextStyle}>VS</Text>
-            </View>
-            <View style={styles.headerViewStyle}>
-              <Text style={styles.headerTextStyle}>When</Text>
-            </View>
-          </CardSection>
-          <FlatList
-            style={{ flex: 1 }}
-            data={this.props.upcomingGames}
-            renderItem={item => this.renderItem(item)}
-          />
-        </Card>
+      <View style={{ flex: 1 }}>
+        <CardSection>
+          <View style={styles.leftViewStyle}>
+            <Text style={styles.headerTextStyle}>Sport</Text>
+          </View>
+          <View style={styles.middleViewStyle}>
+            <Text style={styles.headerTextStyle}>VS</Text>
+          </View>
+          <View style={styles.rightViewStyle}>
+            <Text style={styles.headerTextStyle}>When</Text>
+          </View>
+        </CardSection>
+        <FlatList
+          style={{ flex: 1 }}
+          data={this.props.upcomingGames}
+          renderItem={item => this.renderItem(item)}
+        />
       </View>
     );
   }
 }
 
 const styles = {
-  headerViewStyle: {
-    flex: 1
+  leftViewStyle: {
+    flex: 1,
+    borderRightWidth: 0.5
+  },
+  rightViewStyle: {
+    flex: 1,
+    borderLeftWidth: 0.5
+  },
+  middleViewStyle: {
+    flex: 1,
+    borderLeftWidth: 0.5,
+    borderRightWidth: 0.5
   },
   headerTextStyle: {
-    fontSize: 18,
+    fontSize: 28,
     fontWeight: 'bold',
     color: 'black',
     textAlign: 'center'
+  },
+  bodyTextStyle: {
+    fontSize: 16,
+    color: 'black',
+    textAlign: 'center'
+  },
+  bodyCardSectionStyle: {
+    borderBottomWidth: 0
   }
 };
 
