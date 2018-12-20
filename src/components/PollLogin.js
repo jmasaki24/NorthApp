f89307a5b917803027b189d1da28f5569e13896a
petermatts <mpeters0514@gmail.com>
2018-12-20 08:34:33 -0500

    Login for voting works (using temp ids from a json file), will move to redux shortly

--- src/components/PollLogin.js
+++ src/components/PollLogin.js
@@ -1,10 +1,77 @@
 import React, { Component } from 'react';
 import { SafeAreaView, Text } from 'react-native';
-import { Card, CardSection, Input, Button } from './common';
+import { withNavigation } from 'react-navigation';
+import { Card, CardSection, Input, Button, Spinner } from './common';
+import IDnums from '../JSON/TempID.json';
 
 class PollPage extends Component {
+  state = { ID: '', IDmatches: null, alreadyVoted: null, loading: false };
+
+  authLogin() {
+    const len = IDnums.length;
+    this.setState({ loading: true });
+
+    for (let i = 0; i < len; i++) {
+      if (IDnums[i].ID === this.state.ID) {
+        this.setState({ IDmatches: true, loading: false, alreadyVoted: IDnums[i].hasVoted });
+        break;
+      }
+      this.setState({ IDmatches: false });
+    }
+    this.setState({ loading: false, ID: '' });
+  }
+
+  nav() {
+    if (this.state.IDmatches === true && this.state.alreadyVoted === false) {
+      this.props.navigation.navigate('PollPage');
+    }
+  }
+
+  errorMes() {
+    console.log(this.state);
+    if (this.state.alreadyVoted) {
+      return (
+        <CardSection>
+          <Text>
+            You cannot vote again
+          </Text>
+        </CardSection>
+      );
+    } else if (this.state.IDmatches === false) {
+      return (
+        <CardSection>
+          <Text>
+            Sorry, couldn't login for reasons
+          </Text>
+        </CardSection>
+      );
+    }
+  }
+
+  renderButton() {
+    if (this.state.loading) {
+      return (
+        <CardSection>
+          <Spinner />
+        </CardSection>
+      );
+    }
+    return (
+      <CardSection>
+        <Button
+          buttonStyle={styles.buttonStyle}
+          textStyle={{ color: 'black' }}
+          onPress={() => this.authLogin()}
+        >
+          Login
+        </Button>
+      </CardSection>
+    );
+  }
+
   render() {
-    const { headerStyle, buttonStyle } = styles;
+    const { headerStyle } = styles;
+    this.nav();
     return (
       <SafeAreaView>
         <Card>
@@ -17,15 +84,11 @@ class PollPage extends Component {
             label='ID#'
             placeholder='000000000'
             keyboardType='number-pad'
+            onChangeText={(text) => this.setState({ ID: text })}
+            value={this.state.ID}
           />
-          <CardSection>
-            <Button
-              buttonStyle={buttonStyle}
-              textStyle={{ color: 'black' }}
-            >
-              Login
-            </Button>
-          </CardSection>
+          {this.renderButton()}
+          {this.errorMes()}
         </Card>
       </SafeAreaView>
     );
@@ -36,7 +99,8 @@ const styles = {
   headerStyle: {
     fontSize: 28,
     fontWeight: 'bold',
-    textAlign: 'center'
+    textAlign: 'center',
+    color: 'black'
   },
   buttonStyle: {
     justifyContent: 'center',
@@ -44,4 +108,4 @@ const styles = {
   }
 };
 
-export default PollPage;
+export default withNavigation(PollPage);
--- src/components/PollPage.js
+++ src/components/PollPage.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
+import { Card, CardSection } from './common';
 
 class PollPage extends Component {
   render() {
-    return null;
+    return (
+      <Card>
+        <CardSection>
+          <Text>
+            hello voter
+          </Text>
+        </CardSection>
+      </Card>
+    );
   }
 }
 
